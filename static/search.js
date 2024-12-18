4f5851e3ee369afdd58eb9a67cc34e09e4c12cf6
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-12-18 23:01:34 +0000

    feat: added search feature for wiki articles

---
+++ static/search.js
@@ -0,0 +1,14 @@
+let search = document.getElementById('wiki-search')
+let articles = document.querySelectorAll(".article-item")
+let bigList = document.querySelector('ul')
+// console.log(articles)
+
+search.addEventListener('keyup', function() {
+  bigList.innerHTML = ''
+  for (var article of articles) {
+    if (article.id.toLowerCase().includes(search.value.toLowerCase())) {
+      bigList.appendChild(article)
+    }
+  }
+  // console.log(filteredArticles)
+})
